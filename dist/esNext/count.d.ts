4efb2d29f5981fbd54638c0c99a887ffb35eb439
dennisat <dennis@anel.co>
2020-06-07 11:33:59 +0200

    Build

--- dist/esNext/count.d.ts
+++ dist/esNext/count.d.ts
@@ -1,8 +1,8 @@
 export declare const countFromTo: (from: number, to: number, step?: number) => {
-    for: (cb: (index: number, cbBreak: any) => void) => void;
+    for: (cb: (index: number, cbBreak: () => void) => void) => void;
     map: <T = any>(cb: (index: number) => T) => T[];
 };
 export declare const count: (countTo: number, step?: number) => {
-    for: (cb: (index: number, cbBreak: any) => void) => void;
+    for: (cb: (index: number, cbBreak: () => void) => void) => void;
     map: <T = any>(cb: (index: number) => T) => T[];
 };
